f7c149346368f38bbbd3667003d254e3cb526d06
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-04-14 22:42:07 -0300

    Added injection dependency for use Brand object in others methods

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -7,12 +7,16 @@ use Illuminate\Http\Request;
 
 class BrandController extends Controller
 {
+    public function __construct(Brand $brand) {
+        $this->brand = $brand;
+    }
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        return Brand::all();
+        return $this->brand->all();
     }
 
     /**
@@ -20,32 +24,37 @@ class BrandController extends Controller
      */
     public function store(Request $request)
     {
-        $brand = Brand::create($request->all());
+        $brand = $this->brand->create($request->all());
         return $brand;
     }
 
     /**
      * Display the specified resource.
+     * $id integer
      */
-    public function show(Brand $brand)
+    public function show($id)
     {
-        return $brand;
+        return $this->brand->find($id);
     }
 
     /**
      * Update the specified resource in storage.
+     * integer $id
      */
-    public function update(Request $request, Brand $brand)
+    public function update(Request $request, $id)
     {
+        $brand = $this->brand->find($id);
         $brand->update($request->all());
         return $brand;
     }
 
     /**
      * Remove the specified resource from storage.
+     * $id integer
      */
-    public function destroy(Brand $brand)
+    public function destroy($id)
     {
+        $brand = $this->brand->find($id);
         $brand->delete();
         return ['cod' => 204, 'msg' => 'Registro deletado com sucesso!'];
     }
