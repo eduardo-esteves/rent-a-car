b07578192ac0fa3a248e25b1ab0aa17f8437c7c0
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-04-27 22:50:04 -0300

    developed validation rules to the save and update operation to VehicleModel

--- app/Models/VehicleModel.php
+++ app/Models/VehicleModel.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Validator;
 
 class VehicleModel extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['brand_id', 'model', 'img', 'num_ports', 'lugares', 'air_bag', 'abs'];
+
+    public function validationFields($fields)
+    {
+        $validator = Validator::make($fields, [
+            'brand_id'  => 'exists:brands,id',
+            'model'     => 'required|unique:vehicle_models|min:3',
+            'img'       => 'required|file|mimes:png,jpg,jpeg|max:2048',
+            'num_ports' => 'required|integer|digits_between:1,5',
+            'lugares'   => 'required|integer|digits_between:1,20',
+            'air_bag'   => 'required|boolean',
+            'abs'       => 'required|boolean',
+        ]);
+
+        return $validator;
+    }
 }
