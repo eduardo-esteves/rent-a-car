fb53d53befa0ef453bbb83db17e32d8ebc97d4e4
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-04-28 20:19:54 -0300

    refactored validationFields() function by replacing the array parameter to request object, I also added a condition to verify if the current request is a PATCH HTTP verb if true change the mode of the validation

--- app/Models/VehicleModel.php
+++ app/Models/VehicleModel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 class VehicleModel extends Model
@@ -12,18 +13,32 @@ class VehicleModel extends Model
 
     protected $fillable = ['brand_id', 'model', 'img', 'num_ports', 'lugares', 'air_bag', 'abs'];
 
-    public function validationFields($fields)
+    public function validationFields(Request $request)
     {
-        $validator = Validator::make($fields, [
+        $fields = $request->all();
+        $requestMethod = $request->method();
+        $final_rules = null;
+
+        $rules = [
             'brand_id'  => 'exists:brands,id',
-            'model'     => 'required|unique:vehicle_models|min:3',
+            'model'     => ($requestMethod !== 'POST') ? 'required|min:3' : 'required|unique:vehicle_models|min:3',
             'img'       => 'required|file|mimes:png,jpg,jpeg|max:2048',
             'num_ports' => 'required|integer|digits_between:1,5',
             'lugares'   => 'required|integer|digits_between:1,20',
             'air_bag'   => 'required|boolean',
             'abs'       => 'required|boolean',
-        ]);
+        ];
 
-        return $validator;
+        if ($requestMethod === 'PATCH') {
+            foreach( $fields as $field => $value) {
+                if (array_key_exists($field, $rules)) {
+                    $final_rules[$field] = $rules[$field];
+                }
+            }
+        }
+
+        $final_rules = $final_rules ?? $rules;
+
+        return  Validator::make($fields, $final_rules);
     }
 }
