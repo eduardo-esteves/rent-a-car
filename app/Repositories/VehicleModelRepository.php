c1fba37baadf79a8364b8561d0dfe9d6744c412a
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-05-18 21:53:45 -0300

    refactored Brand and VehicleModel repositories by moving the most part the logic to the new AbstractRepository class

--- app/Repositories/VehicleModelRepository.php
+++ app/Repositories/VehicleModelRepository.php
@@ -2,31 +2,12 @@
 
 namespace App\Repositories;
 
-use Illuminate\Http\Request;
-use Illuminate\Database\Eloquent\Model;
 
-class VehicleModelRepository
+class VehicleModelRepository extends AbstractRepository
 {
-    public function __construct(Model $model)
-    {
-        $this->model = $model;
-    }
-
-    public function selectBrandAttributesWithVehicles(array $attributes): void
+    public function selectVehicleAttributesWithBrands(array $attributes): void
     {
         $this->model = $this->model->selectRaw($attributes['vehicles_attributes']);
         $this->model = $this->model->with($attributes['brands_attributes']);
     }
-
-    public function filters(Request $request): void
-    {
-        $conditions = explode(':', $request->input('filter'));
-        $this->model = $this->model->where($conditions[0], $conditions[1], $conditions[2]);
-    }
-
-    public function getResult(): \Illuminate\Database\Eloquent\Collection
-    {
-        return $this->model->get();
-    }
-
 }
