6c069041ed59e96d3819e2a6c612f97716755dbb
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-05-23 19:41:21 -0300

    edited migration added constraint in field already existing in cars table

--- database/migrations/2023_05_18_221858_alter_table_cars_add_vehicle_model_id_constraint.php
+++ database/migrations/2023_05_18_221858_alter_table_cars_add_vehicle_model_id_constraint.php
@@ -11,7 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::table('cars', function (Blueprint $table) {
+            $table->foreign('vehicle_model_id')->references('id')->on('vehicle_models');
+        });
     }
 
     /**
@@ -19,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::table('cars', function (Blueprint $table) {
+            $table->dropForeign('vehicle_model_id');
+        });
     }
 };
